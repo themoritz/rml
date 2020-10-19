1a769beb8f56dd36a49c8a723e5e93a41beb2666
Moritz Drexl <mdrexl@fastmail.fm>
2020-10-19 14:53:39 -0700

    implement grad for matrices

--- src/ad.rs
+++ src/ad.rs
@@ -149,7 +149,7 @@ pub mod vec {
 
     type T = ArrayD<f64>;
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     enum Expr {
         LitVec { value: T },
         AddVec { left: NodeIndex, right: NodeIndex },
@@ -159,7 +159,7 @@ pub mod vec {
         Var { name: String },
     }
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     struct Node {
         expr: Expr,
         z: T,
@@ -294,28 +294,67 @@ pub mod vec {
             let n = self.graph.node_weight_mut(output).unwrap();
             n.w = T::ones(n.z.shape());
             self.graph.reverse();
+            let mut result: HashMap<String, T> = HashMap::new();
             for ix in toposort(&self.graph, None).unwrap() {
                 let n = self.node(&ix).clone();
                 let w = &n.w;
-                match &n.expr {
-                    Expr::LitVec { value } => {}
-                    Expr::AddVec { left, right } => {}
-                    Expr::MultMat { mat, vec } => {}
-                    Expr::Sigma { vec } => {}
-                    Expr::Relu { vec } => {}
-                    Expr::Var { name } => {}
-                }
+                match n.expr {
+                    Expr::LitVec { .. } => {}
+                    Expr::AddVec { left, right } => {
+                        self.graph.node_weight_mut(left).unwrap().w += w;
+                        self.graph.node_weight_mut(right).unwrap().w += w;
+                    }
+                    Expr::MultMat { mat, vec } => {
+                        let deriv: T = self
+                            .node(&mat)
+                            .z
+                            .t()
+                            .into_dimensionality::<Ix2>()
+                            .unwrap()
+                            .dot(&w.clone().into_dimensionality::<Ix1>().unwrap())
+                            .into_dyn();
+                        self.graph.node_weight_mut(vec).unwrap().w += &deriv;
 
+                        let deriv: T = outer_product(
+                            w.clone().into_dimensionality::<Ix1>().unwrap(),
+                            self.node(&vec)
+                                .z
+                                .clone()
+                                .into_dimensionality::<Ix1>()
+                                .unwrap(),
+                        )
+                        .into_dyn();
+                        self.graph.node_weight_mut(mat).unwrap().w += &deriv;
+                    }
+                    Expr::Sigma { vec } => {
+                        let deriv: T = w * &self.node(&vec).z.map(sigma_deriv);
+                        self.graph.node_weight_mut(vec).unwrap().w += &deriv;
+                    }
+                    Expr::Relu { vec } => {
+                        let deriv: T = w * &self.node(&vec).z.map(relu_deriv);
+                        self.graph.node_weight_mut(vec).unwrap().w += &deriv;
+                    }
+                    Expr::Var { name } => {
+                        result.insert(name.clone(), n.w);
+                    }
+                }
             }
             self.graph.reverse();
-            let mut result: HashMap<String, T> = HashMap::new();
             result
         }
     }
 
-    pub fn example() -> (Tape, T) {
+    pub fn outer_product(a: Array1<f64>, b: Array1<f64>) -> Array2<f64> {
+        let a_len = a.len();
+        let b_len = b.len();
+        a.into_shape((a_len, 1))
+            .unwrap()
+            .dot(&b.into_shape((1, b_len)).unwrap())
+    }
+
+    pub fn example() -> (Tape, T, HashMap<String, T>) {
         let mut t = Tape::init();
-        let a0 = t.lit(array![1.0, 2.0].into_dyn());
+        let a0 = t.lit(array![1.0, 2.0, 3.0].into_dyn());
         let w1 = t.var("w1");
         let b1 = t.var("b1");
         let m1 = t.mult_mat(w1, a0);
@@ -329,9 +368,10 @@ pub mod vec {
         // let a2 = t.sigma(z2);
 
         let result = t.eval(vec![
-            (w1, array![[1.0, 1.0], [2.0, 2.0]].into_dyn()),
+            (w1, array![[1.0, 1.0, 1.0], [2.0, 2.0, 2.0]].into_dyn()),
             (b1, array![1.0, 1.0].into_dyn()),
         ]);
-        (t, result)
+        let grad = t.grad(a1);
+        (t, result, grad)
     }
 }
